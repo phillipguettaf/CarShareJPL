af556b49c4c3756245f1b638302f6a5d8053c81a
Blaise Saunders <davidorotho>
2019-03-29 11:14:13 +1100

    Map moved to own component

---
+++ src/JPLMap.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { ReactBingmaps } from 'react-bingmaps';
+import 'react-geolocation';
+
+
+class JPLMap extends Component
+{
+	state = {
+		response: '',
+		post: '',
+		responseToPost: '',
+	};
+
+	constructor()
+	{
+		super()
+	}
+
+
+	render(props)
+	{
+		return (
+			<div className="Bingmapcont">
+				<ReactBingmaps
+					bingmapKey = 'Ak4YC0ivePGISt6hRJCxFzEeCw67C2dnZV5lPncBzK7v4FOPaHjGrbbIoeww90mP'>
+				</ReactBingmaps>
+			</div>
+		);
+	}
+}
+
+export default JPLMap;
