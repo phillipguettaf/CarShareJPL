650897a3094f05d8c1f053de4fdafc5c32e66366
PritchardGSD <PritchardGSD@users.noreply.github.com>
2019-03-31 23:04:52 +1100

    Refactored map-relevant code into correct component. Test pushpin.

--- src/JPLMap.js
+++ src/JPLMap.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { ReactBingmaps } from 'react-bingmaps';
-import 'react-geolocation';
-
 
 class JPLMap extends Component
 {
@@ -11,11 +9,34 @@ class JPLMap extends Component
 		response: '',
 		post: '',
 		responseToPost: '',
+		latitude: null,
+		longitude: null,
+		error: null,
+
+		pushPins : [
+			{
+			  "location":[-37.8136, 144.9631], "option":{ color: 'red' } //melbourne cbd
+			}
+		],
 	};
 
 	constructor()
 	{
 		super()
+	}
+
+	componentDidMount() {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+			this.setState({
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude,
+				error: null,
+			});
+			},
+			(error) => this.setState({error: error.message}),
+			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+		);
 	}
 
 
@@ -25,7 +46,8 @@ class JPLMap extends Component
 			<div className="Bingmapcont">
 				<ReactBingmaps
 					bingmapKey = 'Ak4YC0ivePGISt6hRJCxFzEeCw67C2dnZV5lPncBzK7v4FOPaHjGrbbIoeww90mP'
-					center = {[this.props.lat, this.props.long]} >
+					center = {[this.state.lat, this.state.long]} 
+					pushPins = {this.state.pushPins}>
 				</ReactBingmaps>
 			</div>
 		);
