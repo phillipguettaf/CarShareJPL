12548934e9f0b9afb468dea049629fff8b64bf6e
Phill <s3607153@student.rmit.edu.au>
2019-04-17 09:34:25 +1000

    Map icon added

--- src/navbar.js
+++ src/navbar.js
@@ -13,7 +13,7 @@ function NavBar(props) {
 		  		<Link to='/splash'><HomeIcon/></Link>
 		  	</Button>
 		    <Button>
-		  		<Link to='/map'><img src="./map.svg"></img></Link>
+		  		<Link to='/map'><MapIcon/></Link>
 		  	</Button>
 	    </Toolbar>
 	</AppBar>
@@ -26,6 +26,15 @@ function HomeIcon(props) {
       <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
     </SvgIcon>
   );
+}
+
+//	This is an icon because React apparently doesn't like images being passed as icons which is rude
+function MapIcon(props) {
+	return (
+	<SvgIcon {...props}>
+		<path d="M12 10.9c-.61 0-1.1.49-1.1 1.1s.49 1.1 1.1 1.1c.61 0 1.1-.49 1.1-1.1s-.49-1.1-1.1-1.1zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm2.19 12.19L6 18l3.81-8.19L18 6l-3.81 8.19z"/><path d="M0 0h24v24H0z" fill="none"/>
+	</SvgIcon>
+	);
 }
 
 export default NavBar;
