1f04472a5c00ee4e2ad1dbf1f7f34bf96d6c3a8d
PritchardGSD <jack.github.inbox@gmail.com>
2019-05-13 17:13:39 +1000

    test passes again with mock data

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 const mockGeolocation = {
     getCurrentPosition: jest.fn(),
-    watchPosition: jest.fn()
+    watchPosition: jest.fn(),
+    clearWatch: jest.fn(),
   };
   
   global.navigator.geolocation = mockGeolocation;
