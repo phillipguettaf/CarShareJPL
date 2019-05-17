ae47ebeaba8b46a5e0b3076700e29544975ed8a2
PritchardGSD <PritchardGSD@users.noreply.github.com>
2019-05-17 12:03:51 +1000

    basic test exploration which does not work. yay

---
+++ src/CarList.test.js
@@ -0,0 +1,13 @@
+import { shallow, mount, render } from 'enzyme';
+import React from 'react';
+import CarList from './CarList';
+
+const wrapper = shallow(<CarList />);
+
+it('can return a list of cars', () => {
+    /* expect(bestLaCroixFlavor()).toBe('grapefruit'); */
+});
+
+it('displays booking buttons for cars', () => {
+   
+});
