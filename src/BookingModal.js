d0447abf57daaef045df84892466a9922caf1307
PritchardGSD <PritchardGSD@users.noreply.github.com>
2019-05-19 19:57:17 +1000

    Booking Modal text fixed

--- src/BookingModal.js
+++ src/BookingModal.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Dialog } from 'evergreen-ui';
 
+import './BookingModal.css'
+
 class BookingModal extends Component {
 	constructor(props) {
 		super(props);
