426b187ba9697554445f9b4ac3fc53c5018c058a
DavidoRotho <derpydeponson@gmail.com>
2019-06-21 15:04:21 +1000

    Fixed admin console

--- src/App.js
+++ src/App.js
@@ -16,14 +16,15 @@ class App extends Component {
 		this.state = {
 			sideBarShown: false,
 		};
-		this.handleUsername = this.handleUsername.bind(this);
+        this.handleUsername = this.handleUsername.bind(this);
+        this.enable_admin = this.enable_admin.bind(this);
 		var user = JSON.parse(localStorage.getItem('userstate'))
 		console.log(localStorage)
 		this.showSideBar = this.showSideBar.bind(this);
 		this.hideSideBar = this.hideSideBar.bind(this);
 		if (user) {
 			console.log("Found user in localstorage already: "+user.name)
-			this.state = { userName: user.name, userEmail: user.email, sideBarShown: false };
+			this.state = { userName: user.name, userEmail: user.email, sideBarShown: false, admin: false };
 		}
 	}
 
@@ -39,31 +40,63 @@ class App extends Component {
         });
     }
 
-  handleUsername(data) {
+    handleUsername(data) {
     this.setState({
-      userName: data.name,
-      userEmail: data.email
+        userName: data.name,
+        userEmail: data.email
     });
-  }
+    }
 
+    enable_admin()
+    {
+        let out;
+        if (this.state.admin)
+            out = false;
+        else
+            out = true;
+        this.setState({
+            admin: out
+        });
+    }
+
+
+    // TODO: Tidy
     render() {
+        
         if (this.state.userName) {
-            return (
-                
-                  <div className="App">
-                      <Router>
-                          <NavBar showSideBar={this.showSideBar} handleUsername={this.handleUsername} />
-                          <MapApp sideBarShown={this.state.sideBarShown} hideSideBar={this.hideSideBar} user={this.state.userEmail}/>
-                      </Router>
-                  </div>
+            // Render the admin page if requested
+            if (this.state.admin)
+            {
+                return (
+                    
+                    <div className="App">
+                        <Router>
+                            <NavBar admin={this.state.admin} enableAdmin={this.enable_admin} showSideBar={this.showSideBar} handleUsername={this.handleUsername} />
+                            <Admin/>
+                        </Router>
+                    </div>
                 
             );
+            }
+            else
+            {
+                return (
+                    
+                    <div className="App">
+                        <Router>
+                        <NavBar enableAdmin={this.enable_admin} showSideBar={this.showSideBar} handleUsername={this.handleUsername} />
+                            <MapApp admin={this.state.admin} sideBarShown={this.state.sideBarShown} hideSideBar={this.hideSideBar} user={this.state.userEmail}/>
+                        </Router>
+                    </div>
+                    
+                );
+            }
         } else {
             return (
               
                     <div className="App">
                         <Router>
-                        <NavBar showSideBar={this.showSideBar} handleUsername={this.handleUsername} />
+                        <NavBar admin={this.state.admin} enableAdmin={this.enable_admin} showSideBar={this.showSideBar} handleUsername={this.handleUsername} />
                         <SplashScreen />
                       </Router>
                     </div>
--- src/navbar.js
+++ src/navbar.js
@@ -3,13 +3,15 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/IconButton';
 import Menu from '@material-ui/icons/Menu';
+import Build from '@material-ui/icons/Build';
+import ArrowBack from '@material-ui/icons/ArrowBack';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import LoginScreen from './LoginScreen'
 import './navbar.css';
 
 class NavBar extends Component {
 
-constructor(props) {
+	constructor(props) {
 		super(props);
 	}
 
@@ -18,17 +20,25 @@ constructor(props) {
 	}
 
 	render(props) {
+
+		let icon;
+		if (this.props.admin)
+			icon = <ArrowBack />;
+		else
+			icon = <Build />;
+
 		return (
 			<AppBar position="static" color="primary">
-		    	<Toolbar>
-			  		<Button onClick={() => this.showSideBar()}>
-            <Button>
-		  		<Link to='/admin'>{/* <MapIcon/> */}</Link>
-		  	</Button>
-			  			<Menu/>
-			  		</Button>
-                    <LoginScreen nameHandler={this.props.handleUsername} />
-                </Toolbar>
+				<Toolbar>
+						
+						<Button onClick={() => this.showSideBar()}>
+							<Menu/>
+						</Button>
+						<Button onClick={() => this.props.enableAdmin()}>
+							{icon}
+						</Button>
+						<LoginScreen nameHandler={this.props.handleUsername} />
+				</Toolbar>
 			</AppBar>
 		);
 	}
